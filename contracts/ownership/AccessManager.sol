79f9b05fce1310e333841800dd9a3797ed481188
eirlis <abandonedsense@gmail.com>
2017-09-23 21:07:34 +0300

    storage, security and lib implementing

---
+++ contracts/ownership/AccessManager.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.4.11;
+
+import "./Owned.sol";
+import "../Dispatcher.sol";
+
+contract AccessManager is Owned {
+
+    uint8 public version = 1;
+
+    Dispatcher public dispatcher;
+
+    function AccessManager(address dispatcherAddress) {
+        setDispatcher(dispatcherAddress);
+    }
+
+    function setDispatcher(address dispatcherAddress) onlyOwner {
+        if (dispatcherAddress == 0x0) throw;
+        dispatcher = Dispatcher(dispatcherAddress);
+    }
+
+    function checkCoinEmissionAccess(address _address) constant returns (bool) {
+        if (_address == 0x0) return false;
+        return _address == dispatcher.getContract("CoinGenerator");
+    }
+
+    function checkCoinTransferAccess(address _address) constant returns (bool) {
+        if (_address == 0x0) return false;
+        return _address == dispatcher.getContract("SellOffer");
+    }
+
+    function checkStorageAccess(address _address) constant returns (bool) {
+        if (_address == 0x0) return false;
+        return _address == dispatcher.getContract("CoinGenerator") || _address == dispatcher.getContract("SellOffer");
+    }
+}
