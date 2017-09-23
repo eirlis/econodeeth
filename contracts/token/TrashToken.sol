a99e1b7891b06ea26f71ab6b92d019f2d0465643
user <user@MacBook-Air-air.local>
2017-09-23 17:29:32 +0300

    create base Trash token

---
+++ contracts/token/TrashToken.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.11;
+
+import "./Token.sol";
+
+contract TrashToken is Token {
+
+    public string mesurements;
+    public bool danger;
+
+    function TrashToken(string _name, string _symbol, uint8 _decimals, string _mesurements, bool _danger)
+             Token(_name, _symbol, _decimals, 0) {
+	mesurements = _mesurements;
+	danger = _danger;
+    }
+}
