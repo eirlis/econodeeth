47fc69fd6539eef9a3fce53aa21ec1cd67c90938
eirlis <abandonedsense@gmail.com>
2017-09-23 22:22:50 +0300

    Warehouse contract added

---
+++ contracts/market/Warehouse.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.11;
+
+import "../ownership/Owned.sol";
+import "../storage/DataStorage.sol";
+import "../storage/TrashRequestDAO.sol";
+import "../token/TrashToken.sol";
+
+
+contract Warehouse is Owned {
+
+	using TrashRequestDAO for address;
+
+	
+
+	function Warehouse (ctorArgs) {
+		
+	}	
+}
