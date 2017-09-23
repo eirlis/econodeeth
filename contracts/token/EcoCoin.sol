09a5ba3d85ae5fc77ef9dff8d903fb034816c287
eirlis <abandonedsense@gmail.com>
2017-09-23 20:06:16 +0300

    Token event for EcoCoin added

--- contracts/token/EcoCoin.sol
+++ contracts/token/EcoCoin.sol
@@ -4,6 +4,11 @@ import './Token.sol';
 
 contract EcoCoin is Token {
 
+    event TokensGenerated(address user, uint256 amount);
+
+    event TokensBurnt(address user, uint256 amount);
+    
+
     function EcoCoin(uint _start_count)
              Token("EcoCoin", "ECT", 18, _start_count) {}
 
@@ -13,12 +18,14 @@ contract EcoCoin is Token {
 
         totalSupply     += _value;
         balances[owner] += _value;
+        TokensGenerated(owner, _value);
     }
   
     function burn(uint _value) {
         if (balances[msg.sender] >= _value) {
             balances[msg.sender] -= _value;
             totalSupply      -= _value;
+            TokensBurnt(msg.sender, _value);
         }
     }
 }
