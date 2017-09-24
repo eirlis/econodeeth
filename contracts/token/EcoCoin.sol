d37b43271a8daaa4699b989b1dc29a0521d70c25
user <user@MacBook-Air-air.local>
2017-09-24 09:41:33 +0300

    change EcoCoin emission beneficiar

--- contracts/token/EcoCoin.sol
+++ contracts/token/EcoCoin.sol
@@ -1,24 +1,33 @@
 pragma solidity ^0.4.11;
 
 import './Token.sol';
+import "../Dispatcher.sol";
 
 contract EcoCoin is Token {
 
     event TokensGenerated(address user, uint256 amount);
-
     event TokensBurnt(address user, uint256 amount);
-    
 
-    function EcoCoin(uint _start_count)
-             Token("EcoCoin", "ECT", 18, _start_count) {}
+    Dispatcher public dispatcher;    
+
+    function EcoCoin(uint _start_count, address dispatcherAddress)
+             Token("EcoCoin", "ECT", 18, _start_count) {
+	setDispatcher(dispatcherAddress);
+    }
+
+    function setDispatcher(address dispatcherAddress) onlyOwner {
+        if (dispatcherAddress == 0x0) throw;
+        dispatcher = Dispatcher(dispatcherAddress);
+    }
 
     function emission(uint _value) onlyOwner {
         // Overflow check
         if (_value + totalSupply < totalSupply) throw;
 
-        totalSupply     += _value;
-        balances[owner] += _value;
-        TokensGenerated(owner, _value);
+	address beneficiar = dispatcher.getContract("Exchange");
+        totalSupply += _value;
+        balances[beneficiar] += _value;
+        TokensGenerated(beneficiar, _value);
     }
   
     function burn(uint _value) {
